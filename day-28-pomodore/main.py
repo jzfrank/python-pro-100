ece7c9868be7cf683676cbe331080f69408ec8b1
jzfrank <frank.jin.zhang@outlook.com>
2022-03-31 09:35:57 +0200

    added day28 pomodore

--- day-28-pomodore/main.py
+++ day-28-pomodore/main.py
@@ -12,26 +12,40 @@ LONG_BREAK_MIN = 20
 
 reps = 0
 
+timer = None
+
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_timer():
+
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer", fg=GREEN)
+    checkbox_label.config(text="")
+    global reps
+    reps = 0
+
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+
 def start_timer():
     global reps
     reps += 1
     if reps % 2 == 1:
+        timer_label.config(text="Work", fg=GREEN)
         count_down(WORK_MIN * 60)
     else:
         if reps % 4 == 0:
+            timer_label.config(text="Break", fg=RED)
             count_down(LONG_BREAK_MIN * 60)
         else:
+            timer_label.config(text="Break", fg=PINK)
             count_down(SHORT_BREAK_MIN * 60)
-
-
-def reset_timer():
-    canvas.itemconfig(timer_text, text="Press Start to start")
 
 
 def count_down(count):
@@ -42,11 +56,14 @@ def count_down(count):
     print("count: ", count)
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count-1)
+        global timer
+        timer = window.after(1000, count_down, count-1)
     elif count == 0:
-        checkbox_label.config(
-            text=checkbox_label.cget("text") + "√"
-        )
+        if reps % 2 == 1:
+            checkbox_label.config(
+                text=checkbox_label.cget("text") + "√"
+            )
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -55,16 +72,11 @@ if __name__ == "__main__":
     window.title("Pomodoro")
     window.config(padx=100, pady=50, bg=YELLOW)
 
-
-
     canvas = Canvas(width=200, heigh=224, bg=YELLOW, highlightthickness=0)
     tomato_img = PhotoImage(file="tomato.png")
     canvas.create_image(100, 112, image=tomato_img)
     timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
     canvas.grid(column=1, row=1)
-
-
-
 
     timer_label = Label(text="Timer" , bg=YELLOW, fg=GREEN, font=(FONT_NAME, 40, "bold"))
     timer_label.grid(column=1, row=0)
@@ -75,8 +87,7 @@ if __name__ == "__main__":
     reset_button = Button(text="reset", highlightbackground=YELLOW, command=reset_timer)
     reset_button.grid(column=2, row=2)
 
-    checkbox_label = Label(text="√", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+    checkbox_label = Label(text="", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
     checkbox_label.grid(column=1, row=3)
-
 
     window.mainloop()
