086469d47d9d5c2acbb30a4b5c15b74ed1c59b1c
jzfrank <frank.jin.zhang@outlook.com>
2022-04-09 22:17:10 +0200

    added day 38

---
+++ day-38-habit-tracking-with-google-sheet/main.py
@@ -0,0 +1,78 @@
+import os
+import requests
+import json
+import datetime as dt
+
+
+NLP_API_ID = os.environ["API_ID"]
+NLP_API_KEY = os.environ["API_KEY"]
+NLP_URL = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+NLP_HEADERS = {
+    "x-app-id": NLP_API_ID,
+    "x-app-key": NLP_API_KEY
+}
+
+
+def exercise_text_nlp(exercise_text="ran 3 miles"):
+
+    params = {
+        "query": exercise_text
+    }
+
+    response = requests.post(NLP_URL, json=params, headers=NLP_HEADERS)
+    response.raise_for_status()
+    print(json.dumps(response.json(), indent=4))
+
+    return response.json()
+
+
+SHEETY_URL = "https://api.sheety.co/28317aaac2e1b81628c20171aa1d3eb9/myFakeWorkout/workouts"
+
+
+def write_to_sheet(record):
+    '''
+    example of record: 
+    {"date": "22/03/2022",
+                    "time": "12:00",
+                    "exercise": "Swimming",
+                    "duration": "19",
+                    "calories": "100"}
+    '''
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    params = {
+        "workout":
+        record
+    }
+
+    # response = requests.get(sheety_url)
+    response = requests.post(SHEETY_URL, json=params, headers=headers)
+    response.raise_for_status()
+    print(json.dumps(response.json(), indent=4))
+
+
+def delete_record(row_num):
+    response = requests.delete(f"{SHEETY_URL}/{row_num}")
+    response.raise_for_status()
+    # print(json.dumps(response.json(), indent=4))
+
+
+if __name__ == "__main__":
+    data = exercise_text_nlp(input("What have you exercised today?\n"))
+    record = {
+        "date": dt.datetime.now().strftime("%d/%m/%y"),
+        "time": dt.datetime.now().strftime("%H:%M:%S"),
+        "exercise": "",
+        "duration": "",
+        "calories": ""
+    }
+    for exercise in data["exercises"]:
+        record["exercise"] = exercise["user_input"]
+        record["duration"] = exercise["duration_min"]
+        record["calories"] = exercise["nf_calories"]
+        print(record)
+        write_to_sheet(record)
